692a508edd27a87cc1ef8d479eb6ff1513c1224d
Tom Holder <taholder@gmail.com>
2021-02-15 16:58:55 -0800

    Target blank

--- app/src/SwapTap/SwapTap.js
+++ app/src/SwapTap/SwapTap.js
@@ -30,7 +30,7 @@ export class ClaimSwap extends React.Component {
                         </div>
                         <div className="ml-4">
                         <div className="text-sm py-2 whitespace-nowrap leading-8">
-                            <p><a href={process.env.REACT_APP_STELLAR_TRANS_URL + trans.transaction_hash} targt="_blank">{trans.transaction_hash.substring(0,13)}... <img src="images/open-external.svg" className="inline-block" alt="Open external site" /></a></p>
+                            <p><a href={process.env.REACT_APP_STELLAR_TRANS_URL + trans.transaction_hash} target="_blank">{trans.transaction_hash.substring(0,13)}... <img src="images/open-external.svg" className="inline-block" alt="Open external site" /></a></p>
                         </div>
                         </div>
                     </div>
@@ -53,13 +53,13 @@ export class ClaimSwap extends React.Component {
                       </button>
                       ) : trans.mined ? (
                         <div>  
-                          <a href={process.env.REACT_APP_ETHER_TRANS_URL + trans.ethTrans} targt="_blank">
+                          <a href={process.env.REACT_APP_ETHER_TRANS_URL + trans.ethTrans} target="_blank">
                           <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="check-circle" className="inline-block mr-2 h-3.5 svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#1110A7" d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path></svg>
                           Claied {trans.ethTrans.substring(0,13)}...<img src="images/open-external.svg" className="inline-block ml-2 h-3.5" alt="Open external site" /></a>
                         </div>
                       ) : (
                         <div>  
-                          <a className={buttonClasses.replace('bg-tap_blue', 'bg-tap_orange')} href={process.env.REACT_APP_ETHER_TRANS_URL + trans.ethTrans} targt="_blank">
+                          <a className={buttonClasses.replace('bg-tap_blue', 'bg-tap_orange')} href={process.env.REACT_APP_ETHER_TRANS_URL + trans.ethTrans} target="_blank">
                           <svg className="animate-spin -ml-1 mr-2 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="#ffffff" strokeWidth="4"></circle>
                             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
